c04c9f0180e3197a1492924b27438ddb9e6032d9
Claire Daley <clairedaley123@icloud.com>
2026-02-12 15:02:13 -0700

    fix front page and background.

--- src/ranking/ranking.jsx
+++ src/ranking/ranking.jsx
@@ -2,5 +2,43 @@ import React from 'react';
 import './ranking.css';
 
 export function Ranking() {
-  return <main className="container">Ranking page content goes here</main>;
+  return (
+    <div class="ranking_body">
+        <div class="ranking_header">
+            <header>
+                <h1>Rankings</h1>
+            </header>
+        </div>
+
+        <div class="jokeAPI">
+            <h2><a href="https://sv443.net/jokeapi/v2/">jokeAPI HERE</a></h2>
+        </div>
+
+        <div class="ranks">
+            <div class="gold row">
+                <span class="rank">🏆 1</span>
+                <span class="name">PlayerOne</span>
+                <span class="score">125,000</span>
+            </div>
+
+            <div class="silver row">
+                <span class="rank">🏆 2</span>
+                <span class="name">PlayerTwo</span>
+                <span class="score">110,000</span>
+            </div>
+
+             <div class="bronze row">
+                <span class="rank">🏆 3</span>
+                <span class="name">PlayerThree</span>
+                <span class="score">100,000</span>
+            </div>
+
+            <div class="gold row">
+                <span class="rank">4</span>
+                <span class="name">PlayerFour</span>
+                <span class="score">90,000</span>
+            </div>
+        </div>
+    </div>
+  )
 }
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -2,5 +2,73 @@ import React from 'react';
 import './store.css';
 
 export function Store() {
-  return <main className="container">Store page content goes here</main>;
+  return (
+    <div class="storebody">
+        <div class="storeheader">
+            <header>
+                <h1>Store</h1>
+            </header>
+        </div>
+
+    <div class="points_available">
+        <p>
+            🪙 Scores: <span id="countscore">0</span>
+        </p>
+    </div>
+
+        <div class="buy_buttons">
+            <h2>Button Skins</h2>
+            <div class="btn_items">
+                <button class="options">
+                    <div class="base">
+                        <div class="blue_button"></div>
+                    </div>
+                    100
+                </button>
+
+                <button class="options">
+                    <div class="base">
+                        <div class="neon_button"></div>
+                    </div>
+                    200
+                </button>
+
+                <button class="options">
+                    <div class="base">
+                        <div class="gold_button"></div>
+                    </div>
+                    300
+                </button>
+                <button class="options">
+                    <div class="base">
+                        <div class="fire_button"></div>
+                    </div>
+                    500
+                </button>
+            </div>
+        </div>
+
+        <div class="buy_backgrounds">
+            <h2>Backgrounds</h2>
+            <div class="back_options">
+                <button class="options">
+                        <div class="forest_background"></div>
+                    1000
+                </button>
+                <button class="options">
+                    <div class="beach_background"></div>
+                    2000
+                </button>
+                <button class="options">
+                    <div class="space_background"></div>
+                    3000
+                </button>
+                <button class="options">
+                    <div class="city_background"></div>
+                    5000
+                </button>
+            </div>
+        </div>
+    </div>
+  )
 }
